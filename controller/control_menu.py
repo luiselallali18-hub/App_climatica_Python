71ae552bd8c20371718880165ff1156aa2e98bf2
adryeli <99803754+adryeli@users.noreply.github.com>
2026-04-07 15:28:58 +0200

    Update control_menu.py

--- controller/control_menu.py
+++ controller/control_menu.py
@@ -1,5 +1,18 @@
-OPCIONES_VALIDAS = {"1", "2", "3"}
+ZONAS = {
+    "1": "Centro",
+    "2": "Norte",
+    "3": "Sur",
+    "4": "Este",
+    "5": "Oeste"
+}
 
 
 def validar_opcion_menu(opcion):
-    return opcion in OPCIONES_VALIDAS
+    return opcion in {"1", "2", "3"}
+
+
+def validar_opcion_zona(opcion):
+    """
+    Valida si la opción de zona es correcta.
+    """
+    return opcion in ZONAS
